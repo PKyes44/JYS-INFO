31a0ca9487968ae88fc2d4621b1b0200b8f2c5fc
PKyes44 <rugate23@gmail.com>
2023-08-19 22:51:35 +0900

    universityView need to redevelop

--- jysInfo/src/main/java/com/jys/jysInfo/MainController.java
+++ jysInfo/src/main/java/com/jys/jysInfo/MainController.java
@@ -1,7 +1,13 @@
 package com.jys.jysInfo;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 import java.util.Map;
@@ -21,20 +27,28 @@ public class MainController {
         return "MajorInfo";
     }
     @GetMapping("/universityInfo")
-    public String universityInfo() {
+    public String universityInfo(Model model) {
+        return "UniversityInfo";
+    }
 
+    @ResponseBody
+    @PostMapping(value = {"/universityInfo"})
+    public List<Map<String, String>> universityExcelSearch(HttpServletRequest req, HttpServletResponse res,
+                                                           @RequestParam(defaultValue = "0") int page,
+                                                           @RequestParam(defaultValue = "20") int size) {
         ReadOption ro = new ReadOption();
-        ro.setFilePath("C:\\Users\\Yang\\OneDrive\\바탕 화면\\포폴제출용\\JYS-INFO\\jysInfo\\src\\main\\resources\\static\\입학전형유형별선발결과현황.xlsx");
-        ro.setOutputColumns("A", "B");
+        ro.setFilePath("C:\\Users\\rugat\\OneDrive\\바탕 화면\\JYS-Information\\jysInfo\\src\\main\\resources\\static\\입학전형유형별선발결과현황.xlsx");
+        ro.setOutputColumns("A","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U");
         ro.setStartRow(1);
 
         List<Map<String, String>> result = ExcelRead.read(ro);
 
-        for(Map<String, String> map : result) {
-            System.out.println(map.get("A"));
-        }
+        // 페이징 처리를 위해 데이터를 sublist로 추출
+        int startIdx = page * size;
+        int endIdx = Math.min(startIdx + size, result.size());
+        List<Map<String, String>> pageData = result.subList(startIdx, endIdx);
 
-        return "UniversityInfo";
+        return pageData;
     }
 
 }
--- jysInfo/src/test/java/com/jys/jysInfo/JysInfoApplicationTests.java
+++ jysInfo/src/test/java/com/jys/jysInfo/JysInfoApplicationTests.java
@@ -4,6 +4,10 @@ import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 @SpringBootTest
 class JysInfoApplicationTests {
 
@@ -11,14 +15,31 @@ class JysInfoApplicationTests {
 	void contextLoads() {
 	}
 
-	@Resource
-	FirebaseService firebaseService;
-
 	@Test
 	public void Test() throws Exception {
-		Member member = Member.builder()
-				.id("0")
-				.name("user1").build();
-		firebaseService.getUserDetails(member);
+		ReadOption ro = new ReadOption();
+		ro.setFilePath("C:\\Users\\rugat\\OneDrive\\바탕 화면\\JYS-Information\\jysInfo\\src\\main\\resources\\static\\입학전형유형별선발결과현황.xlsx");
+		ro.setOutputColumns("A","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U");
+		ro.setStartRow(1);
+
+		List<List<String>> result = ExcelRead.read(ro);
+		List<String> columns = new ArrayList<String>();
+		List<List<String>> rows = new ArrayList<>();
+
+//		for (String col : ro.getOutputColumns()) {
+//			// Map 상태의 Column을 List로 변환
+//			Map<String, String> tempColumns = result.get(0);
+//			columns.add(tempColumns.get(col));
+//		}
+//		for (int i = 1; i < result.size(); i++) {
+//			List<String> row = new ArrayList<String>();
+//			for (String col : ro.getOutputColumns()) {
+//				Map<String, String> tempRows = result.get(i);
+//				row.add(tempRows.get(col));
+//			}
+//			rows.add(row);
+//		}
+		System.out.println("columns = " + columns);
+		System.out.println("rows = " + rows);
 	}
 }
