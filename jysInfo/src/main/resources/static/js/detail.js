0f7925658f1acbc94208cb17c0fce67d4810e250
PKyes44 <rugate23@gmail.com>
2023-09-05 15:31:51 +0900

    20230905-4

--- jysInfo/src/main/resources/static/js/detail.js
+++ jysInfo/src/main/resources/static/js/detail.js
@@ -2,6 +2,32 @@ google.charts.load('current', {'packages':['bar']});
 google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
+    $.ajax({
+            url: '/universityInfo',  // 요청할 URL
+            type: 'post',      // 요청 타입 (GET, POST 등)
+            dataType: 'json', // 응답 데이터 타입 (JSON, XML 등)
+            data: data,
+            async: true, //비동기 여부
+            timeout: 10000, //타임 아웃 설정 (1000 = 1초)
+            success: function(response) {
+                // 서버 응답 성공 시 실행되는 콜백 함수
+                // response 변수에 서버에서 받은 데이터가 담겨 있음
+                // 원하는 작업 수행
+                console.log(response["tableData"])
+                console.log("totalCount : " + response["dataCount"])
+
+                pageData = response['tableData'];
+                totalCount = response['dataCount'];
+                pageSize = response['pageSize'];
+                totalPage = Math.ceil(totalCount / pageSize);
+                setList(page);
+            },
+            error: function(xhr, status, error) {
+                // 서버 요청 실패 시 실행되는 콜백 함수
+                // 오류 처리 등을 수행
+            }
+        });
+
     var data = google.visualization.arrayToDataTable([
         ['연도', '수시등록률(%)', '정시등록률(%)', '추가등록률(%)', '최종등록률(%)'],
         ['2014', 1000, 400, 200, 20],
